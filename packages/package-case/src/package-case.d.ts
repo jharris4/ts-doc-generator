c005f4496eb040e825e0ee187dfaad83541d877a
jharris4 <harris.jb@gmail.com>
2022-11-07 11:39:39 -0500

    add package-case example package

---
+++ packages/package-case/src/package-case.d.ts
@@ -0,0 +1,34 @@
+/**
+ * package-case summary goes here
+ *
+ * @remarks
+ *
+ * More detailed description about package-case
+ * @packageDocumentation
+ */
+
+/**
+ * summary for WithCaseClashInterface
+ *
+ * @remarks
+ *
+ * Interface with case clash on property names
+ */
+export interface WithCaseClashInterface {
+  property: string;
+  Property: number;
+}
+
+/**
+ * Lowercase class name
+ */
+export class aclass {
+  lowerFunction(): void;
+}
+
+/**
+ * Uppercase first letter class name
+ */
+export class Aclass {
+  upperFunction(): void;
+}
